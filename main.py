e9736619d28c759f04e96b34fe9233114f4700c4
YaroslavKSE <ikhomych@kse.org.ua>
2023-10-19 09:28:26 +0300

    Created simple Api example which displays Hello world

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    @staticmethod
+    def get():
+        return {'Hello' : 'world'}, 200
+
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
